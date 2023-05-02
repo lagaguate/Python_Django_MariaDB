dc127c25d30ee8961122f2fc72267ae7593090f1
lagaguate <lagaguate@yahoo.com>
2023-05-02 12:00:42 -0600

    Test envio de correos

--- TiendaOnline/settings.py
+++ TiendaOnline/settings.py
@@ -134,3 +134,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+# En Gmail, permite enviar correos desde terceros
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS=True
+EMAIL_PORT = 587
+EMAIL_HOST_USER="lagaguate@gmail.com"
+EMIAL_HOST_PASSWORD="ferbeth19721976**"
+
+
--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from TiendaOnline import settings
+from gestionPedidos.models import Articulos
+from django.core.mail import send_mail
+
 
 # Create your views here.
 
@@ -7,6 +11,33 @@ def busqueda_productos(request):
     return render(request, "busqueda_productos.html")
 
 def buscar(request):
-    mensaje ="Articulo buscado :%r" %request.GET["prd"]
 
+    if request.GET["prd"]:
+        #mensaje ="Articulo buscado :%r" %request.GET["prd"]
+        producto  = request.GET["prd"]
+        if len(producto)>20: 
+            mensaje = "Texto muy largo"
+        else:     
+            articulos = Articulos.objects.filter (nombre__icontains = producto )
+            return render(request, "resultados_busqueda.html", {"articulos":articulos, "query":producto})
+    else: 
+       mensaje = "Debe indicar el valor"
+
+       
+    
     return HttpResponse(mensaje)
+
+def contacto (request):
+    if request.method =="POST":
+        subject = request.POST["asunto"]
+        message = request.POST["mensaje"] + " " + request.POST["email"]
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = ["lgutiegt@gmail.com"]
+        print(subject) 
+        print(message)
+        print(email_from) 
+        send_mail(subject, message, email_from, recipient_list)
+
+        return render(request, "gracias.html")
+    
+    return render(request, "contacto.html")
